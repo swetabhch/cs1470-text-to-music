0348af997d1ea70f7d34b53bf4cf9646fe083e1c
Swetabh Changkakoti <50801120+swetabhch@users.noreply.github.com>
2024-04-30 22:57:58 -0400

    setting up backbone for model stuff

---
+++ code/positional_encoding.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+
+def positional_encoding(length, depth):
+    ## REFERENCE: https://www.tensorflow.org/text/tutorials/transformer#the_embedding_and_positional_encoding_layer
+    ## TODO: Can remove signature
+    depth = depth / 2
+    ## Generate a range of positions and depths
+    positions = np.arange(length)[:, np.newaxis]  # (seq, 1)
+    depths = np.arange(depth)[np.newaxis, :] / depth  # (1, depth)
+    ## Compute range of radians to take the sine and cosine of.
+    angle_rates = 1 / (10000**depths)  # (1, depth)
+    angle_rads = positions * angle_rates  # (pos, depth)
+    pos_encoding = np.concatenate([np.sin(angle_rads), np.cos(angle_rads)], axis=-1)
+    ## This serves as offset for the Positional Encoding
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class PositionalEncoding(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, embed_size, window_size):
+        super().__init__()
+        self.embed_size = embed_size
+
+        ## TODO: Implement Component
+
+        ## Embed labels into an optimizable embedding space
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embed_size)
+
+        ## Implement sinosoidal positional encoding: offset by varying sinosoidal frequencies.
+        ## HINT: May want to use the function above...
+        self.pos_encoding = positional_encoding(length=window_size, depth=embed_size)
+
+    def call(self, x):
+        ## TODO: Get embeddings and and scale them by sqrt of embedding size, and add positional encoding.
+        length = tf.shape(x)[1]
+        embedded_input = self.embedding(x)
+        scaled_input = embedded_input * tf.math.sqrt(
+            tf.cast(self.embed_size, tf.float32)
+        )
+        pos_encoding = self.pos_encoding[:length, :]
+        out = scaled_input + pos_encoding
+        return out
---
+++ code/transformer.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import tensorflow_models as tfm
+
+from positional_encoding import PositionalEncoding
+
+
+class TransformerDecoder(tf.keras.Model):
+
+    def __init__(self, vocab_size, hidden_size, window_size, **kwargs):
+
+        super().__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.window_size = window_size
+
+        # Define feed forward layer(s) to embed image features into a vector
+        self.audio_embedding = tf.keras.layers.Dense(
+            self.hidden_size, activation="relu"
+        )
+
+        # Define positional encoding to embed and offset layer for language:
+        self.encoding = PositionalEncoding(vocab_size, hidden_size, window_size)
+
+        # Define transformer decoder layer:
+        # self.decoder = TransformerBlock(hidden_size)
+        self.decoder = tfm.nlp.layers.TransformerEncoderBlock(inner_dim=hidden_size)
+
+        # Define classification layer(s) (LOGIT OUTPUT)
+        self.classifier = tf.keras.layers.Dense(self.vocab_size)
+
+    def call(self, encoded_audio, captions):
+        # TODO:
+        # 1) Embed the encoded images into a vector (HINT IN NOTEBOOK)
+        # 2) Pass the captions through your positional encoding layer
+        # 3) Pass the english embeddings and the image sequences to the decoder
+        # 4) Apply dense layer(s) to the decoder out to generate **logits**
+        audio_enc = self.audio_embedding(tf.expand_dims(encoded_audio, 1))
+        captions_enc = self.encoding(captions)
+        decoder_out = self.decoder(captions_enc, audio_enc)
+        logits = self.classifier(decoder_out)
+        return logits
