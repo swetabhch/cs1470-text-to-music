567ca69743ae0e5bf17be127d8b11f9411a7b244
Swetabh Changkakoti <50801120+swetabhch@users.noreply.github.com>
2024-05-03 18:00:34 -0400

    uncommented parse_args

--- code/main.py
+++ code/main.py
@@ -21,58 +21,57 @@ def parse_args(args=None):
     For example:
         parse_args('--type', 'rnn', ...)
     """
-    # parser = argparse.ArgumentParser(
-    #     description="Let's train some neural nets!",
-    #     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    # )
-    # parser.add_argument(
-    #     "--type",
-    #     required=True,
-    #     choices=["rnn", "transformer"],
-    #     help="Type of model to train",
-    # )
-    # parser.add_argument(
-    #     "--task", required=True, choices=["train", "test", "both"], help="Task to run"
-    # )
-    # parser.add_argument(
-    #     "--data", required=True, help="File path to the assignment data file."
-    # )
-    # parser.add_argument(
-    #     "--epochs", type=int, default=3, help="Number of epochs used in training."
-    # )
-    # parser.add_argument("--lr", type=float, default=1e-3, help="Model's learning rate")
-    # parser.add_argument(
-    #     "--optimizer",
-    #     type=str,
-    #     default="adam",
-    #     choices=["adam", "rmsprop", "sgd"],
-    #     help="Model's optimizer",
-    # )
-    # parser.add_argument(
-    #     "--batch_size", type=int, default=100, help="Model's batch size."
-    # )
-    # parser.add_argument(
-    #     "--hidden_size",
-    #     type=int,
-    #     default=256,
-    #     help="Hidden size used to instantiate the model.",
-    # )
-    # parser.add_argument(
-    #     "--window_size", type=int, default=20, help="Window size of text entries."
-    # )
-    # parser.add_argument(
-    #     "--chkpt_path", default="", help="where the model checkpoint is"
-    # )
-    # parser.add_argument(
-    #     "--check_valid",
-    #     default=True,
-    #     action="store_true",
-    #     help="if training, also print validation after each epoch",
-    # )
-    # if args is None:
-    #     return parser.parse_args()  ## For calling through command line
-    # return parser.parse_args(args)  ## For calling through notebook.
-    pass
+    parser = argparse.ArgumentParser(
+        description="Let's train some neural nets!",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--type",
+        required=True,
+        choices=["rnn", "transformer"],
+        help="Type of model to train",
+    )
+    parser.add_argument(
+        "--task", required=True, choices=["train", "test", "both"], help="Task to run"
+    )
+    parser.add_argument(
+        "--data", required=True, help="File path to the assignment data file."
+    )
+    parser.add_argument(
+        "--epochs", type=int, default=3, help="Number of epochs used in training."
+    )
+    parser.add_argument("--lr", type=float, default=1e-3, help="Model's learning rate")
+    parser.add_argument(
+        "--optimizer",
+        type=str,
+        default="adam",
+        choices=["adam", "rmsprop", "sgd"],
+        help="Model's optimizer",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=100, help="Model's batch size."
+    )
+    parser.add_argument(
+        "--hidden_size",
+        type=int,
+        default=256,
+        help="Hidden size used to instantiate the model.",
+    )
+    parser.add_argument(
+        "--window_size", type=int, default=20, help="Window size of text entries."
+    )
+    parser.add_argument(
+        "--chkpt_path", default="", help="where the model checkpoint is"
+    )
+    parser.add_argument(
+        "--check_valid",
+        default=True,
+        action="store_true",
+        help="if training, also print validation after each epoch",
+    )
+    if args is None:
+        return parser.parse_args()  ## For calling through command line
+    return parser.parse_args(args)  ## For calling through notebook.
 
 
 def main(args):
